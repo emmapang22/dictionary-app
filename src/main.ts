7b617827b99c75c029d4d3ad6f0029fffe0b5f42
Emma Pang <emmapang3@gmail.com>
2025-11-25 20:11:11 +0100

    integrated the form with the user input

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
+import { getWords } from "./services/dictionaryService";
 import "./style.css";
+
+document.getElementById("searchForm")?.addEventListener("submit", async (e) => {
+  e.preventDefault;
+
+  const theInput = document.getElementById("searchText");
+
+  let searchText = "";
+
+  if (theInput) {
+    searchText = (theInput as HTMLInputElement).value;
+  }
+
+  const words = await getWords(searchText);
+  createHtml(words);
+
+  if (theInput) {
+    (theInput as HTMLInputElement).value = "";
+  }
+});
